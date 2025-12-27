e79a7ada7574e3e3e15310c2106846feb8f4b88b
anjanmondal <anjan.gk.mondal@gmail.com>
2025-12-27 21:47:54 +0530

    Add more metrics

--- accuracy.py
+++ accuracy.py
@@ -2,12 +2,30 @@ import numpy as np
 import yaml
 
 
-def load_array(path: str) -> np.array:
+def load_array(path: str) -> np.ndarray:
     return np.loadtxt(path, delimiter=",")
 
 
+def mse(y_true, y_pred):
+    return np.mean((y_true - y_pred) ** 2)
+
+
+def rmse(y_true, y_pred):
+    return np.linalg.norm(y_true - y_pred) / np.sqrt(len(y_pred))
+
+
 def accuracy(y_true, y_pred):
+    if not np.issubdtype(y_true.dtype, np.integer):
+        raise ValueError("Accuracy is only valid for categorical targets")
     return (y_true == y_pred).mean()
+
+
+def mae(y_true, y_pred):
+    return np.mean(np.abs(y_pred - y_true))
+
+
+def mape(y_true, y_pred, eps=1e-7):
+    return np.mean(np.abs((y_pred - y_true) / (y_true + eps))) * 100
 
 
 def main():
@@ -17,11 +35,27 @@ def main():
     y_true = load_array(cfg["y_true"])
     y_pred = load_array(cfg["y_pred"])
 
-    if cfg["metric"] != "accuracy":
-        raise ValueError(f"Unsupported metric: {cfg['metric']}")
+    y_true = np.asarray(y_true).ravel()
+    y_pred = np.asarray(y_pred).ravel()
 
-    score = accuracy(y_true, y_pred)
-    print({"accuracy": float(score)})
+    assert len(y_true) == len(y_pred), (
+        "The ground truth and the predicted arrays must be of the same length!"
+    )
+
+    METRICS = {
+        "mse": mse,
+        "rmse": rmse,
+        "accuracy": accuracy,
+        "mae": mae,
+        "mape": mape,
+    }
+
+    metric = cfg["metric"]
+    if metric not in METRICS:
+        raise ValueError(f"Unsupported metric: {metric}")
+
+    score = METRICS[metric](y_true, y_pred)
+    print({metric: float(score)})
 
 
 if __name__ == "__main__":
